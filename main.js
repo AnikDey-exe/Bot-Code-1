e97cf8ff79ca09dae950c9bdc7945bf69369edce
AnikDey-exe <67157750+AnikDey-exe@users.noreply.github.com>
2021-01-31 12:41:31 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const botsettings = require('./botsettings.json');
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+    console.log('Cryptic Clan bot is online!'); 
+})
+
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    let prefix = botsettings.prefix;
+    let messageArray = message.content.split(" ")
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    
+
+    if(cmd === `${prefix}command goes here`){
+        return message.reply("reply goes here")
+    }
+
+})
+
+bot.login(process.env.token);
